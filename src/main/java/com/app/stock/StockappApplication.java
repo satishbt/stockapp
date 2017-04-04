2808b9f341d6a35f35542f3eb6b9a5bb2880d64c
satlight <satishbtgsl@gmail.com>
2017-04-04 16:52:47 +0530

    Application is launched

---
+++ src/main/java/com/app/stock/StockappApplication.java
@@ -0,0 +1,12 @@
+package com.app.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockappApplication.class, args);
+	}
+}
